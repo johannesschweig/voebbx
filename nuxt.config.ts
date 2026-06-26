b9dbbe123be39d4681ce17b691e66181f02e56b8
Johannes Schweig <j.schweig@posteo.de>
2026-06-26 10:55:21 +0200

    favicon lightning

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'BibBlitz',
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/svg+xml',
+          // %E2%9A%A1 is the code for the ⚡ emoji
+          href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>%E2%9A%A1</text></svg>'
+        }
+      ]
+    }
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxtjs/supabase'],
